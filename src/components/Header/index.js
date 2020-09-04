a4588e51a68ba61dfa071e7df730c92981f22f02
Mohammad Moien <mnmoienian@gmail.com>
2020-09-04 20:36:01 +0430

    add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <div>
+      <table className="titleBar">
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="./images/green_app_icon.svg"
+                alt="app logo"
+                width="50"
+              />
+            </td>
+            <td width="8"></td>
+            <td>
+              <h1>MoviesDB Search</h1>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
