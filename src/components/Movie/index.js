9e8fe704d8694620088f63bfe1e8fe2a836c6f8e
Mohammad Moien <mnmoienian@gmail.com>
2020-09-09 16:03:46 +0430

    add view button for view movie page on movieDB site

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,25 +1,33 @@
 import React from "react";
+import "./Movie.scss";
 
 export default function Movie({ movies }) {
+  function viewMovie(id) {
+    window.location.href = `https://themoviedb.org/movie/${id}`;
+  }
   return (
     <div>
       {movies.map((movie) => (
-        <table key={movie.id}>
-          <tbody>
-            <tr>
-              <td>
-                <img
-                  src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-                  alt="movie poster"
-                />
-              </td>
-              <td>
-                <h3>{movie.title}</h3>
-                <p>{movie.overview}</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="movie-item" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt="movie poster"
+            className="movie-poster"
+          />
+
+          <div className="movie-review">
+            <div>
+              <h2>{movie.title}</h2>
+              <p>{movie.overview}</p>
+            </div>
+            <input
+              type="button"
+              value="View"
+              className="view-btn"
+              onClick={() => viewMovie(movie.id)}
+            />
+          </div>
+        </div>
       ))}
     </div>
   );
